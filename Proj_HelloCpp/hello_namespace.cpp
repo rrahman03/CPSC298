52986993895a55d8008cc4e2f58e4fb0ac5e60f3
Raneem Rahman <123603989+rrahman03@users.noreply.github.com>
2024-02-08 21:17:38 -0800

    added hello_namespace.cpp

---
+++ Proj_HelloCpp/hello_namespace.cpp
@@ -0,0 +1,12 @@
+/// @file hello_namespace.cpp
+/// @brief hello_namespace
+/// @author raneem rahman
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello C++!" << endl;
+    return 0;
+}
+
